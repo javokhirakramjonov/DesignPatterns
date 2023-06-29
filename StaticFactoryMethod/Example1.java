3f3be30759124aa772bdaf452cf907e6d008effb
Javokhir <javokhirakromjonov@gmail.com>
2023-06-29 03:20:18 +0200

    Create Example1.java

---
+++ StaticFactoryMethod/Example1.java
@@ -0,0 +1,8 @@
+public class Example1 {
+    public static void main(String[] args) {
+        Long x = Long.valueOf("123");
+        Long y = Long.valueOf("123");
+
+        System.out.println(x == y);//always true
+    }
+}
